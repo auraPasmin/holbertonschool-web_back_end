fba0ba13510e285b25d1a59407f6a81c1396b49f
auraPasmin <1288@holbertonschool.com>
2020-10-20 00:15:59 -0500

    fix

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -7,7 +7,7 @@ from typing import Union, Any, Mapping, Tuple, List
 
 def zoom_array(lst: Tuple, factor: int = 2) -> List:
     """
-    Use mypy to validate the following piece of code and apply any necessary changes.
+    zoom
     """
     zoomed_in: List = [
         item for item in lst
