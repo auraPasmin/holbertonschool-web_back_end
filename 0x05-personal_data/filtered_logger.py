da27c690ee784f6ca9165cee842940eeac9471cf
auraPasmin <1288@holbertonschool.com>
2020-11-14 03:10:26 -0500

    add task 1

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -6,12 +6,35 @@ from typing import List
 import logging
 
 
-def filter_datum(fields: List[str],
-                 redaction: str,
-                 message: str,
-                 separator: str) -> str:
-    """Regex-ing"""
-    for item in fields:
-        message = re.sub(fr'{item}=.+?{separator}',
-                         f'{item}={redaction}{separator}', message)
-    return message
+class RedactingFormatter(logging.Formatter):
+    """
+    Redacting Formatter class
+    """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """
+        Constructor
+        """
+        self.fields = fields
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+
+    def format(self, record: logging.LogRecord) -> str:
+        """
+        format function
+        """
+        return filter_datum(self.fields, self.REDACTION,
+                            super().format(record), self.SEPARATOR)
+
+
+def get_logger() -> logging.getLogger:
+    """
+    get_logger function
+    """
+    logging.(filename='user_data', level=logging.INFO)
+
+
+def filter_datum(fields: List[str], redaction: str, message: str,
