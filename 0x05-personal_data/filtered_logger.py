fcbb3705b0ff0b0ac2b5b2f9caddc95fe30d80e1
auraPasmin <1288@holbertonschool.com>
2020-11-14 03:18:14 -0500

    I forgot to upload the files

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
+"""0x05. Personal data"""
+
 import re
 from typing import List
 import logging
+import mysql.connector
+from os import getenv
+
+PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
 
 
 def filter_datum(fields: List[str],
@@ -34,3 +40,61 @@ class RedactingFormatter(logging.Formatter):
         """
         return filter_datum(self.fields, self.REDACTION,
                             super().format(record), self.SEPARATOR)
+
+
+def get_logger() -> logging.Logger:
+    """Create logger
+        returns a logging.Logger object
+    """
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+
+    handler = logging.StreamHandler()
+    handler.setFormatter(RedactingFormatter(PII_FIELDS))
+    logger.addHandler(handler)
+    return logger
+
+
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    """Connect to secure database
+        returns a connector to the database
+    """
+    db = mysql.connector.connection.MySQLConnection(
+            user=getenv('PERSONAL_DATA_DB_USERNAME', 'root'),
+            password=getenv('PERSONAL_DATA_DB_PASSWORD', ''),
+            host=getenv('PERSONAL_DATA_DB_HOST', 'localhost'),
+            database=getenv('PERSONAL_DATA_DB_NAME'))
+
+    return db
+
+
+def main():
+    """Read and filter data
+    Implement a main function that takes no arguments and returns nothing.
+    """
+    db = get_db()
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM users;")
+
+    result = cursor.fetchall()
+    for data in result:
+        message = f"name={data[0]}; " + \
+                  f"email={data[1]}; " + \
+                  f"phone={data[2]}; " + \
+                  f"ssn={data[3]}; " + \
+                  f"password={data[4]}; " + \
+                  f"ip={data[5]}; " + \
+                  f"last_login={data[6]}; " + \
+                  f"user_agent={data[7]};"
+        print(message)
+        log_record = logging.LogRecord("my_logger", logging.INFO,
+                                       None, None, message, None, None)
+        formatter = RedactingFormatter(PII_FIELDS)
+        formatter.format(log_record)
+    cursor.close()
+    db.close()
+
+
+if __name__ == '__main__':
+    main()
