ba151369513b0d3400cfb168dff001e51b1dbb36
auraPasmin <1288@holbertonschool.com>
2020-11-14 03:13:15 -0500

    fix task 1

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -1,40 +1,36 @@
 #!/usr/bin/env python3
-"""0. Regex-ing"""
-
 import re
 from typing import List
 import logging
 
 
+def filter_datum(fields: List[str],
+                 redaction: str,
+                 message: str,
+                 separator: str) -> str:
+    """Regex-ing"""
+    for item in fields:
+        message = re.sub(fr'{item}=.+?{separator}',
+                         f'{item}={redaction}{separator}', message)
+    return message
+
+
 class RedactingFormatter(logging.Formatter):
-    """
-    Redacting Formatter class
-    """
+    """ Redacting Formatter class
+        """
 
     REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
     def __init__(self, fields: List[str]):
-        """
-        Constructor
-        """
-        self.fields = fields
         super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
-        """
-        format function
+        """Implement the format method to filter values in incoming log
+        records using filter_datum.
+        Values for fields in fields should be filtered
         """
         return filter_datum(self.fields, self.REDACTION,
                             super().format(record), self.SEPARATOR)
-
-
-def get_logger() -> logging.getLogger:
-    """
-    get_logger function
-    """
-    logging.(filename='user_data', level=logging.INFO)
-
-
-def filter_datum(fields: List[str], redaction: str, message: str,
