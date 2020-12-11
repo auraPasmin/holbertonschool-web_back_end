32b33038dfe9b9cf4ddbdb122ede8a1bb7d53b29
auraPasmin <1288@holbertonschool.com>
2020-12-10 22:44:44 -0500

    add db

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -4,21 +4,25 @@ Implement User class in DB
 """
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.exc import NoResultFound
 from user import Base
 from user import User
-from typing import TypeVar
 
 
 class DB:
     """
-    DB class
+        DB class
+        create model to manage the Database
     """
+
     def __init__(self):
         """
-        constructor
+            Constructor
+            create the object BD
         """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        self._engine = create_engine("sqlite:///a.db")
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
@@ -26,21 +30,42 @@ class DB:
     @property
     def _session(self):
         """
-        create session
+            Create if not exists a db session
+            Return the session
         """
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email: str, hashed_password: str) -> TypeVar(User):
+    def add_user(self, email: str, hashed_password: str) -> User:
         """
-        create user
+            Create an User and insert in Database
+            Retrun the new User
         """
-        user = User(
-            email=email,
-            hashed_password=hashed_password
-        )
+        user = User(email=email, hashed_password=hashed_password)
         self._session.add(user)
         self._session.commit()
         return user
+
+    def find_user_by(self, **kwargs) -> User:
+        """
+            Find an user in db
+            Return the user founded
+        """
+        user = self._session.query(User).filter_by(**kwargs).first()
+        if not user:
+            raise NoResultFound
+        return user
+
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """
+            Update a user in database
+            Return None
+        """
+        user = self.find_user_by(id=user_id)
+        for key, value in kwargs.items():
+            if key not in user.__dict__:
+                raise ValueError
+            setattr(user, key, value)
+        return None
